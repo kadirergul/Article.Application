0449ecf2f45e57f25edbed1b55242dc2d8aa766f
kadirergul <kadir.ergul37@gmail.com>
2020-04-06 15:58:52 +0300

    Controller yazıldı.
    Controller yazıldı ve repository pattern için unuttuğum dependecy injectionı yazdım.

---
+++ Article.Application/Controllers/ArticlesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Article.Application.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Article.Application.Controllers
+{
+    [Route("api/[controller]")]
+    public class ArticlesController : Controller
+    {
+        private readonly IRepository _repository;
+
+        public ArticlesController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        
+        public ActionResult GetArticles()
+        {
+            var articles = _repository.Articles().ToList();
+
+            if (articles.Count() > 0){
+                return Ok(articles);
+            }
+            else{
+                return new BadRequestResult();
+            }
+        }
+
+        [HttpPost("{id}")]
+        public ActionResult GetArticleById(int id)
+        {
+            var article = _repository.Article(id);
+
+            if (article != null){
+                return Ok(article);
+            }
+            else {
+                return BadRequest(article);
+            }
+        }
+    }
+}
--- Article.Application/Startup.cs
+++ Article.Application/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using Article.Application.Data;
 
 namespace Article.Application
 {
@@ -28,6 +29,7 @@ namespace Article.Application
         {
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("MyConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<IRepository, Repository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
